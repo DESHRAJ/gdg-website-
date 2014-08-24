793abe6212e1702f285a171ab0ecaf4d1a2ecf8d
deshraj <deshrajdry@gmail.com>
2014-08-24 20:14:19 +0530

    settings file changed

--- gdg/settings.py
+++ gdg/settings.py
@@ -13,10 +13,8 @@ ADMINS = (
      ('deshraj', 'desh.py@gmail.com'),
 )
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
