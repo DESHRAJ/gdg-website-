b553fd19bcd06b2b504dcdcc0d80a20cf4fa52ff
deshraj <deshrajdry@gmail.com>
2014-08-25 16:48:14 +0530

    index changed

--- gdg/views.py
+++ gdg/views.py
@@ -23,26 +23,15 @@ def home(request):
     f1.write(str(count))
     f1.close()
     return render_to_response('index.html', context_instance=RequestContext(request))
-# 	return HttpResponse("This page is under construction")
+
+
 def send_email(request):
     name = request.POST['name']
     message = request.POST['message']
     from_email = request.POST['from_email']
-    print name
-    print message
-    print from_email
     if message and name and from_email:
     	send_mail(from_email, message, from_email, ['desh.py@gmail.com'])
-    	#send_mail('desharaj', 'hello this is deshraj from allahabad', 'great.shivam19@gmail.com', ['deshrajdry@gmail.com'])
-    	return HttpResponse ('thankyou for contacting us ')
-    # if name=='' :
-    #     try:
-    #         send_mail(name, message, from_email, ['deshrajdry@gmail.com'])
-    #     except BadHeaderError:
-    #         return HttpResponse('Invalid header found.')
-    #     return HttpResponseRedirect('/home')
+        return HttpResponse ('thankyou for contacting us ')
     else:
-        # In reality we'd use a form class
-        # to get proper validation errors.
         return HttpResponse('Make sure all fields are entered and valid.')
     return HttpResponseRedirect('')
